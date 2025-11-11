37805536ad018064c546843fd0ef127b5fd6061c
Justin White <jw@justinwhite.net>
2025-11-11 00:34:23 -0500

    tweak vite config for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue"
+import {defineConfig} from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.GITHUB_PAGES === "production" ? "/tide-catcher-vue/" : "/",
   server: {
     port: 3000
+  },
+  preview: {
+    port: 3000,
+  },
+  build: {
+    sourcemap: true,
+    license: true,
   }
 })
