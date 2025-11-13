e77e641438bc1dd68f094d2fe45d88175c9c61a6
Justin White <jw@justinwhite.net>
2025-11-13 10:48:16 -0500

    explicitly handle redirects for the bare id/location paths

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import StationList from "./components/StationList.vue"
 import TidesByLocation from "./components/TidesByLocation.vue"
 import TidesByStation from "./components/TidesByStation.vue"
 import TidesChooser from "./components/TidesChooser.vue"
+import { DEFAULT_LOCATION, DEFAULT_STATION } from "./lib/constants.ts"
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,10 +19,34 @@ export const router = createRouter({
     { path: "/about", component: AboutPage },
     { path: "/tides", component: TidesChooser },
     { path: "/tides/location/:loc", component: TidesByLocation, props: true },
+    {
+      path: "/tides/location/",
+      redirect: () => {
+        return { path: `/tides/location/${DEFAULT_LOCATION}` }
+      },
+    },
     { path: "/tides/station/:id", component: TidesByStation, props: true },
+    {
+      path: "/tides/station/",
+      redirect: () => {
+        return { path: `/tides/station/${DEFAULT_STATION}` }
+      },
+    },
     { path: "/station", component: StationChooser },
     { path: "/station/id/:id", component: StationById, props: true },
+    {
+      path: "/station/id/",
+      redirect: () => {
+        return { path: `/station/id/${DEFAULT_STATION}` }
+      },
+    },
     { path: "/station/location/:loc", component: StationByLocation, props: true },
+    {
+      path: "/station/location/",
+      redirect: () => {
+        return { path: `/station/location/${DEFAULT_LOCATION}` }
+      },
+    },
     { path: "/station/list", component: StationList, props: true },
   ],
 })
